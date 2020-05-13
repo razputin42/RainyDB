69ab51e3e8850f630d85413ad9db6b8fe6b62b29
jeppe4life <jeppe@fribsen.net>
2020-05-13 13:30:26 +0200

    Added and adapted collections from fight club

--- RainyDatabase.py
+++ RainyDatabase.py
@@ -1,29 +1,134 @@
-class RainyDatabase:
-    def load_resources(self, path):
+from RainyCore.item import Item
+from RainyCore.monster import Monster
+from RainyCore.spell import Spell
+import os
+import xml.etree.ElementTree as ElementTree
 
-        self.item_table_widget.load_all("./item", "{}/{}/Items/".format(resource_path, self.version), item_cls)
-        self.item_table_widget.fill_table()
-        self.item_table_widget.define_filters(self.version)
-        self.monster_table_widget.load_all("./monster", "{}/{}/Bestiary/".format(resource_path, self.version), monster_cls)
-        self.monster_table_widget.fill_table()
-        self.monster_table_widget.define_filters(self.version)
-        self.spell_table_widget.load_all("./spell", "{}/{}/Spells/".format(resource_path, self.version), spell_cls)
-        self.spell_table_widget.fill_table()
-        self.spell_table_widget.define_filters(self.version)
+
+class RainyDatabase:
+    def __init__(self, path, include_spells=True, include_items=True, include_monsters=True):
+        self.entries = dict({
+            str(Item):      dict(),
+            str(Monster):   dict(),
+            str(Spell):     dict()
+        })
+        self.path = path
+        self.include_spells = include_spells
+        self.include_items = include_items
+        self.include_monsters = include_monsters
+
+        self.load_resources(path)
+
+    def load_resources(self, path):
+        if self.include_items:
+            self.load_items(path)
+        if self.include_monsters:
+            self.load_monsters(path)
+        if self.include_spells:
+            self.load_spells(path)
+        self.load_collections(path)
+
+        # self.item_table_widget.load_all("./item", "{}/{}/Items/".format(resource_path, self.version), item_cls)
+        # self.item_table_widget.fill_table()
+        # self.item_table_widget.define_filters(self.version)
+        # self.monster_table_widget.load_all("./monster", "{}/{}/Bestiary/".format(resource_path, self.version), monster_cls)
+        # self.monster_table_widget.fill_table()
+        # self.monster_table_widget.define_filters(self.version)
+        # self.spell_table_widget.load_all("./spell", "{}/{}/Spells/".format(resource_path, self.version), spell_cls)
+        # self.spell_table_widget.fill_table()
+        # self.spell_table_widget.define_filters(self.version)
+
+    def load_items(self, path):
+        self.load_all("./item", "{}/5/Items/".format(path), Item)
+
+    def load_monsters(self, path):
+        self.load_all("./monster", "{}/5/Bestiary/".format(path), Monster)
+
+    def load_spells(self, path):
+        self.load_all("./spell", "{}/5/Spells/".format(path), Spell)
+
+    def load_collections(self, path):
+        path = os.path.join(path, "5", "Collections")
+        for resource in os.listdir(path):
+            # print("Before", resource, self.length(Item), self.length(Monster), self.length(Spell))
+            self.load_collection(path, resource)
+
+    def load_collection(self, path, resource):
+        xml = ElementTree.parse(os.path.join(path, resource))
+        root = xml.getroot()
+        # entry_dict = self.entries[str(Class)]
+        if self.include_spells:
+            for itt, entry in enumerate(root.findall("./spell")):
+                self.insert_individual(Spell(entry, itt), self.entries[str(Spell)], resource)
+
+        if self.include_monsters:
+            for itt, entry in enumerate(root.findall("./monster")):
+                self.insert_individual(Monster(entry, itt), self.entries[str(Monster)], resource)
+
+        if self.include_items:
+            for itt, entry in enumerate(root.findall("./item")):
+                self.insert_individual(Item(entry, itt), self.entries[str(Item)], resource)
+
+    # entry_class = Class(entry, itt)
+            # if entry_class.name not in entry_dict.keys():
+            #     entry_dict[entry_class.name] = entry_class
+            # else:
+            #     entry_dict[entry_class.name].append_source(resource)
 
     def load_all(self, s, dir, Class):
-        self.dir = dir
-        self.list = []
         for resource in os.listdir(dir):
-            self.load_list(s, dir + resource, Class)
-        self.list.sort(key=lambda x: x.name)
-        for itt, entry in enumerate(self.list):
-            entry.index = itt
+            self.load_specific(s, dir, resource, Class)
 
-    def load_specific(self, s, resource, Class):
-        xml = ElementTree.parse(resource)
+    def load_specific(self, s, dir, resource, Class):
+        xml = ElementTree.parse(dir + resource)
         root = xml.getroot()
+        entry_dict = self.entries[str(Class)]
         for itt, entry in enumerate(root.findall(s)):
-            self.list.append(Class(entry, itt))
-        self.list_dict[str(Class)] = self.list
+            entry_class = Class(entry, itt)
+            self.insert_individual(entry_class, entry_dict, resource)
 
+    def insert_individual(self, entry, dictionary, resource):
+        if entry.name not in dictionary.keys():
+            dictionary[entry.name] = entry
+        else:
+            if type(entry) is Spell:
+                dictionary[entry.name].append_classes(entry)
+            else:
+                dictionary[entry.name].append_source(resource)
+
+    def list(self, Class):
+        entry_dict = self.entries[str(Class)]
+        for name in entry_dict:
+            print(name)
+
+    def length(self, Class):
+        return len(self.entries[str(Class)])
+
+    def find(self, name, Class):
+        entry_dict = self.entries[str(Class)]
+        if name not in entry_dict.keys():
+            return None
+        else:
+            return entry_dict[name]
+
+    def validate(self):
+        self.validate_individual(Spell)
+        self.validate_individual(Monster)
+        self.validate_individual(Item)
+
+    def validate_individual(self, entry_class):
+        print(str(entry_class))
+        entries = self.entries[str(entry_class)]
+        for name, entry in entries.items():
+            for req in entry.required_database_fields:
+                if not hasattr(entry, req) or getattr(entry, req) is None:
+                    print(entry.name)
+                    return False
+
+
+
+if __name__ == "__main__":
+    db = RainyDatabase(os.getcwd())
+    # db.list(Item)
+    print(db.length(Item), db.length(Monster), db.length(Spell))
+    db.validate()
