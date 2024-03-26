ef9f749fa5813ba3626ac56d994c16baf1c0dd86
Jeppe Friberg <jfr@icepower.dk>
2024-03-26 13:09:28 +0100

    fix: fixed broken item rolling and loot widget behavior

--- sw5e/__init__.py
+++ sw5e/__init__.py
@@ -3,5 +3,5 @@ from ..entry import EntryType
 DatabaseRESTAPI = {
     EntryType.Monster: "https://sw5eapi.azurewebsites.net/api/monster",
     EntryType.Power: "https://sw5eapi.azurewebsites.net/api/power",
-    EntryType.Item: "https://sw5eapi.azurewebsites.net/api/equipment"
+    EntryType.Item: "https://sw5eapi.azurewebsites.net/api/enhancedItem"
 }
